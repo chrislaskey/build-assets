819bc9c026071fdb479709b70cc3d51e2a5b84ba
Chris Laskey <contact@chrislaskey.com>
2013-05-13 14:56:04 -0400

    Cleaned up build-monitor code

--- build-monitor.py
+++ build-monitor.py
@@ -4,36 +4,38 @@ import time
 from datetime import datetime
 from os import path, walk
 
-target_dirs = ["test-dir"]
+class Monitor:
 
-current_time = time.time()
-latest_modified = current_time
+    def __init__(self):
+        current_time = time.time()
+        self.latest_modified = current_time
+        self.target_dirs = ["test-dir"]
 
-def monitor():
-    while True:
-        _trigger_build_if_a_file_has_been_modified()
-        time.sleep(1)
+    def run(self, sleep_seconds = 1):
+        while True:
+            self.trigger_build_if_modified()
+            time.sleep(sleep_seconds)
 
-def _trigger_build_if_a_file_has_been_modified():
-    global target_dirs, latest_modified
-    start_time = time.time()
-    for dir in target_dirs:
-        for file_root, file_dir, files in walk(dir):
-            for filename in files:
-                file = path.join(file_root, filename)
-                if path.exists(file):
-                    last_modified = _get_last_modified(file)
-                    if last_modified > latest_modified:
-                        latest_modified = last_modified
-                        print("The file {0} is modified. Triggered build at {1}".format(file, datetime.now()))
-    end_time = time.time()
-    print("Start: {0}, End: {1}, Difference: {2}".format(start_time, end_time, (end_time - start_time)))
+    def trigger_build_if_modified(self):
+        for dir in self.target_dirs:
+            for file_root, file_dir, files in walk(dir):
+                for filename in files:
+                    file = path.join(file_root, filename)
+                    last_modified = self.get_last_modified(file)
+                    if last_modified > self.latest_modified:
+                        self.latest_modified = last_modified
+                        return self.print_modified_message(file)
 
-def _get_last_modified(file):
-    return path.getmtime(file)
+    def print_modified_message(self, file):
+        print("The file {0} is modified. Triggered build at {1}"
+                .format(file, datetime.now()))
+
+    def get_last_modified(self, file):
+        return path.getmtime(file)
 
 if __name__ == "__main__":
     try:
-        monitor()
+        file_monitor = Monitor()
+        file_monitor.run()
     except KeyboardInterrupt:
         print("Exiting build monitor")
